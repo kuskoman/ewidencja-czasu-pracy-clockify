c132b111f8a11011b2d0b910d0eb729772cd0518
Jakub <kubasurdej@gmail.com>
2023-02-03 16:26:09 +0100

    Create Dockerfile that packs both frontend and backend

--- backend/src/frontend/frontend.module.ts
+++ backend/src/frontend/frontend.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import {
+  ServeStaticModule,
+  ServeStaticModuleOptions,
+} from '@nestjs/serve-static';
 import { join } from 'path';
+import {
+  TemplatesConfig,
+  TEMPLATES_CONFIG_KEY,
+} from '../config/templates.config';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, 'dist'),
-      exclude: ['/api*'],
+    ServeStaticModule.forRootAsync({
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const tempaltesConfig =
+          configService.get<TemplatesConfig>(TEMPLATES_CONFIG_KEY);
+        if (!tempaltesConfig) {
+          throw new Error('Missing templates config');
+        }
+        const serveOptions: ServeStaticModuleOptions = {
+          rootPath: join(__dirname, '..', '..', 'static'),
+        };
+        return [serveOptions];
+      },
     }),
   ],
 })
