be5a191a2009c38c8ca4db92aabbf619faa59d9b
Jakub <jakub.surdej@codahead.com>
2023-02-14 18:40:28 +0100

    Start handling file entry

--- frontend/src/app/report-form/report-form.component.ts
+++ frontend/src/app/report-form/report-form.component.ts
@@ -12,8 +12,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatMomentDateModule } from '@angular/material-moment-adapter';
-import { NgxFileDropModule } from 'ngx-file-drop';
+import {
+  FileSystemFileEntry,
+  NgxFileDropEntry,
+  NgxFileDropModule,
+} from 'ngx-file-drop';
 import { MatIconModule } from '@angular/material/icon';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-report-form',
@@ -30,11 +35,15 @@ import { MatIconModule } from '@angular/material/icon';
     MatMomentDateModule,
     NgxFileDropModule,
     MatIconModule,
+    HttpClientModule,
   ],
   standalone: true,
 })
 export class ReportFormComponent implements OnInit {
   public form!: FormGroup;
+  private file: NgxFileDropEntry | null = null;
+
+  constructor(private readonly http: HttpClient) {}
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -42,7 +51,48 @@ export class ReportFormComponent implements OnInit {
       surname: new FormControl('', [Validators.required]),
       periodStart: new FormControl(''),
       periodEnd: new FormControl(''),
-      file: new FormControl('', [Validators.required]),
+    });
+  }
+
+  public dropped(files: NgxFileDropEntry[]) {
+    if (files.length != 1) {
+      throw new Error('You can only drop a single file');
+    }
+
+    const [file] = files;
+    this.form;
+  }
+
+  public async submit() {
+    console.log('Submitting');
+    const formData = await this.getFormData();
+    this.http
+      .post('http://localhost:3000/reports', formData)
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+
+  private getFormData() {
+    const formData = new FormData();
+
+    Object.entries(formData).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    if (!this.file) {
+      throw new Error('You must define file to submit the form');
+    }
+
+    if (this.file.fileEntry.isDirectory) {
+      throw new Error('You cannot upload a directory');
+    }
+
+    return new Promise((res, rej) => {
+      const fileEntry = this.file!.fileEntry as FileSystemFileEntry;
+      fileEntry.file((loadedFile) => {
+        formData.append('file', loadedFile, this.file!.relativePath);
+      });
     });
   }
 }
