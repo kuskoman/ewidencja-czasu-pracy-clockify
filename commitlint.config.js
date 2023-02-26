5719a0b63f6a557fcc327dfb36f9081cc2fc7682
Jakub <kubasurdej@gmail.com>
2023-02-26 12:24:18 +0100

    chore: remove case requirement from commit subjects

--- commitlint.config.js
+++ commitlint.config.js
@@ -8,7 +8,6 @@ module.exports = {
     "type-case": [2, "always", "lower-case"],
     "type-empty": [2, "never"],
     "scope-case": [2, "always", "lower-case"],
-    "subject-case": [2, "always", "lower-case"],
     "subject-empty": [2, "never"],
     "header-max-length": [2, "always", 80],
   },
